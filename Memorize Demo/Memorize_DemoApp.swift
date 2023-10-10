7f2315b510a4ad73a68f6ecabfdc567b9da9ec31
Bright_Peerapat <Peerapat@example.com>
2023-10-10 15:10:20 +0700

    Initial Commit

---
+++ Memorize Demo/Memorize_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_DemoApp.swift
+//  Memorize Demo
+//
+//  Created by Mac-Peerapat on 10/10/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
